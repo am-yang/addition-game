25df33929e5da6610a4b9f63cffd573604ae7df9
Anita Yang <anita@Anitas-MacBook-Pro-3.local>
2022-12-21 12:49:22 +1100

    Added logic if answer is incorrect

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ document.querySelector(".num2").textContent = num2;
 
 let currScore = 0;
 
-console.log(document.querySelector(".score").textContent);
-
 document.querySelector(".submit-button").addEventListener("click", function () {
-  const computedSum = Number(document.querySelector(".answer").value);
-  if (computedSum == num1 + num2) {
+  let computedSum = Number(document.querySelector(".answer").value);
+  if (computedSum === num1 + num2) {
     currScore++;
     document.querySelector(".score").textContent = currScore;
-    if (currScore > Number(document.querySelector(".score").textContent)) {
-      document.querySelector(".score").textContent = currScore;
+    if (currScore > Number(document.querySelector(".highscore").textContent)) {
+      document.querySelector(".highscore").textContent = currScore;
     }
+  } else if (computedSum !== num1 + num2) {
+    currScore = 0;
+    document.querySelector(".score").textContent = currScore;
   }
+  document.querySelector(".answer").value = ""; // Make answer box empty again
+  num1 = Math.trunc(Math.random() * 100) + 1;
+  num2 = Math.trunc(Math.random() * 100) + 1;
+  document.querySelector(".num1").textContent = num1;
+  document.querySelector(".num2").textContent = num2;
 });
